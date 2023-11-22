c1b8c70abb9807a175a1bb1f6dc552332825b032
Paola-Ake-Cambranis <148833125+Paola-Ake-Cambranis@users.noreply.github.com>
2023-11-21 21:00:01 -0600

    Version 1

---
+++ Ejemplo1/src/Banco.java
@@ -0,0 +1,10 @@
+class Banco {
+  private String nombre_banco;
+
+  public Banco(String nombre) {
+    nombre_banco = nombre;
+  }
+  public String getNombreBanco() {
+    return nombre_banco;
+  }
+}
